2977376abcf973fdca648ecd2a9eab0db2f4fb64
Sasha S.P. <sasa@bubulazy.com>
2024-12-26 13:32:28 +0000

    Tailwind stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,22 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Home Page</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
 
-      <Link
-        href={ticketsPath()}
-        className="underline"
-      >
-        Go to tickets
-      </Link>
+      <div className="flex flex-col flex-1 items-center ">
+        <Link
+          href={ticketsPath()}
+          className="underline"
+        >
+          Go to tickets
+        </Link>
+      </div>
     </div>
   );
 };
