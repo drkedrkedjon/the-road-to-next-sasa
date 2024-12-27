c5af009013d53f59359fba569fde2c853fcd940e
Sasha S.P. <sasa@bubulazy.com>
2024-12-27 01:55:24 +0000

    unsafe-inline for scripts added

--- next.config.ts
+++ next.config.ts
@@ -2,40 +2,41 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // async headers() {
-  //   return [
-  //     {
-  //       // Apply headers to all routes
-  //       source: "/:path*",
-  //       headers: [
-  //         {
-  //           key: "Content-Security-Policy",
-  //           value: "default-src 'self'; script-src 'self'; object-src 'none';",
-  //         },
-  //         {
-  //           key: "Strict-Transport-Security",
-  //           value: "max-age=63072000; includeSubDomains; preload",
-  //         },
-  //         {
-  //           key: "X-Content-Type-Options",
-  //           value: "nosniff",
-  //         },
-  //         {
-  //           key: "X-Frame-Options",
-  //           value: "DENY",
-  //         },
-  //         {
-  //           key: "Referrer-Policy",
-  //           value: "strict-origin-when-cross-origin",
-  //         },
-  //         {
-  //           key: "Permissions-Policy",
-  //           value: "geolocation=(), microphone=(), camera=()",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'unsafe-inline'; object-src 'none';  ",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
