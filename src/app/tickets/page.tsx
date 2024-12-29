e680a0c17c11f13171f1af5b54a69785b5ce4299
Sasha S.P. <sasa@bubulazy.com>
2024-12-29 13:29:08 +0000

    Created features folder and extracted Ticket component there

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,6 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
-
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
-};
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
 const TicketsPage = () => {
   return (
@@ -28,37 +12,10 @@ const TicketsPage = () => {
 
       <div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <Card
+          <TicketItem
             key={ticket.id}
-            className="w-full max-w-[420px]"
-          >
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <span className="truncate">{ticket.title}</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <span className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content +
-                  ticket.content +
-                  ticket.content +
-                  ticket.content +
-                  ticket.content +
-                  ticket.content +
-                  ticket.content +
-                  ticket.content}
-              </span>
-            </CardContent>
-            <CardFooter>
-              <Link
-                href={ticketPath(ticket.id)}
-                className="underline text-sm"
-              >
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+            ticket={ticket}
+          />
         ))}
       </div>
     </div>
