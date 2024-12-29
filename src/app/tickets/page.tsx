40f05e6b5b7a0e96380ce7e2fb7d92198cce60ca
Sasha S.P. <sasa@bubulazy.com>
2024-12-29 18:03:27 +0000

    Nice stuff

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-import { useEffect, useState } from "react";
-
 import { Heading } from "@/components/heading";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { GetTickets } from "@/features/ticket/queries/get-tickets";
 import { Ticket } from "@/features/ticket/types";
 
-const TicketsPage = () => {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const data = await GetTickets();
-      setTickets(data);
-      setLoading(false);
-    };
-
-    fetchTickets();
-  }, []);
+const TicketsPage = async () => {
+  const tickets = await GetTickets();
 
   return (
     <div className="flex-1 flex flex-col gap-y-8">
@@ -28,14 +14,12 @@ const TicketsPage = () => {
       />
 
       <div className="flex flex-1 flex-col items-center gap-y-4 animate-fade-in-from-top">
-        {loading
-          ? "Upsss... Loading"
-          : tickets.map((ticket: Ticket) => (
-              <TicketItem
-                key={ticket.id}
-                ticket={ticket}
-              />
-            ))}
+        {tickets.map((ticket: Ticket) => (
+          <TicketItem
+            key={ticket.id}
+            ticket={ticket}
+          />
+        ))}
       </div>
     </div>
   );
