50d6577067eb286b69662e704b2faaf4730e5ad4
Sasha S.P. <sasa@bubulazy.com>
2024-12-31 17:30:14 +0000

    Initial implementation of add new ticket form

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,14 @@ import { Suspense } from "react";
 
 import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 // export const dynamic = "force-dynamic";
@@ -14,6 +22,17 @@ const TicketsPage = async () => {
         title="Tickets"
         description="All your tickets at one place"
       />
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TicketCreateForm />
+        </CardContent>
+      </Card>
+
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
